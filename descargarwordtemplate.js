d91c0fa5cdacd4537a4a7c817b537033dc189739
alonso0929 <alonso.molina.09@gmail.com>
2023-08-15 16:20:36 -0500

    Se avanzo hasta criterios de aceptacion

--- descargarwordtemplate.js
+++ descargarwordtemplate.js
@@ -41,6 +41,53 @@ function generateDocument() {
     const des_termino10 = document.getElementById('des_termino10').value; 
     const des_termino11 = document.getElementById('des_termino11').value; 
 
+    const tipofuncionalidad1 = document.getElementById('tipofuncionalidad1').value;
+    const tipofuncionalidad2 = document.getElementById('tipofuncionalidad2').value;
+    const tipofuncionalidad3 = document.getElementById('tipofuncionalidad3').value;
+    const tipofuncionalidad4 = document.getElementById('tipofuncionalidad4').value;
+
+    const tipoprueba1 = document.getElementById('tipoprueba1').value;
+    const tipoprueba2 = document.getElementById('tipoprueba2').value;
+    const tipoprueba3 = document.getElementById('tipoprueba3').value;
+    const tipoprueba4 = document.getElementById('tipoprueba4').value;
+
+    const numerousuariocarga1 = document.getElementById('numerousuariocarga1').value;
+    const numerousuariocarga2 = document.getElementById('numerousuariocarga2').value;
+    //const numerousuariocarga3 = document.getElementById('numerousuariocarga3').value;
+    //const numerousuariocarga4 = document.getElementById('numerousuariocarga4').value;
+
+    const tiempopruebacarga1 = document.getElementById('tiempopruebacarga1').value;
+    const tiempopruebacarga2 = document.getElementById('tiempopruebacarga2').value;
+    //const tiempopruebacarga3 = document.getElementById('tiempopruebacarga3').value;
+    //const tiempopruebacarga4 = document.getElementById('tiempopruebacarga4').value;
+
+    const graficocarga1 = document.getElementById('graficocarga1').value;
+    const graficocarga2 = document.getElementById('graficocarga2').value;
+    //const graficocarga3 = document.getElementById('graficocarga3').value;
+    //const graficocarga4 = document.getElementById('graficocarga4').value;
+
+    const numerousuarioestres1 = document.getElementById('numerousuarioestres1').value;
+    const numerousuarioestres2 = document.getElementById('numerousuarioestres2').value;
+    const numerousuarioestres3 = document.getElementById('numerousuarioestres3').value;
+    const numerousuarioestres4 = document.getElementById('numerousuarioestres4').value;
+    const numerousuarioestres5 = document.getElementById('numerousuarioestres5').value;
+    const numerousuarioestres6 = document.getElementById('numerousuarioestres6').value;
+
+    const tiempopruebaestres1 = document.getElementById('tiempopruebaestres1').value;
+    const tiempopruebaestres2 = document.getElementById('tiempopruebaestres2').value;
+    const tiempopruebaestres3 = document.getElementById('tiempopruebaestres3').value;
+    const tiempopruebaestres4 = document.getElementById('tiempopruebaestres4').value;
+    const tiempopruebaestres5 = document.getElementById('tiempopruebaestres5').value;
+    const tiempopruebaestres6 = document.getElementById('tiempopruebaestres6').value;
+
+    const graficoestres1 = document.getElementById('graficoestres1').value;
+    //const graficoestres2 = document.getElementById('graficoestres2').value;
+    //const graficoestres3 = document.getElementById('graficoestres3').value;
+
+    const criterioaceptacion = document.getElementById('criterioaceptacion').value;
+    const graficocriterioaceptacion = document.getElementById('graficocriterioaceptacion').value;
+
+
     if (docs.files.length === 0) {
       alert("No files selected");
     }
@@ -53,6 +100,8 @@ function generateDocument() {
     };
   
     reader.onload = function (evt) {
+      const imageData = evt.target.result;
+
       const content = evt.target.result;
       const zip = new PizZip(content);
       const doc = new window.docxtemplater(zip, {
@@ -97,7 +146,53 @@ function generateDocument() {
         des_termino8: des_termino8,
         des_termino9: des_termino9,
         des_termino10: des_termino10,
-        des_termino11: des_termino11
+        des_termino11: des_termino11,
+
+        tipofuncionalidad1: tipofuncionalidad1,
+        tipofuncionalidad2: tipofuncionalidad2,
+        tipofuncionalidad3: tipofuncionalidad3,
+        tipofuncionalidad4: tipofuncionalidad4,
+
+        tipoprueba1: tipoprueba1,
+        tipoprueba2: tipoprueba2,
+        tipoprueba3: tipoprueba3,
+        tipoprueba4: tipoprueba4,
+
+        numerousuariocarga1: numerousuariocarga1,
+        numerousuariocarga2: numerousuariocarga2,
+        //numerousuariocarga3: numerousuariocarga3,
+        //numerousuariocarga4: numerousuariocarga4,
+
+        tiempopruebacarga1: tiempopruebacarga1,
+        tiempopruebacarga2: tiempopruebacarga2,
+        //tiempopruebacarga3: tiempopruebacarga3,
+        //tiempopruebacarga4: tiempopruebacarga4,
+
+        graficocarga1: graficocarga1,
+        graficocarga2: graficocarga2,
+        //graficocarga3: graficocarga3,
+        //graficocarga4: graficocarga4,
+
+        numerousuarioestres1: numerousuarioestres1,
+        numerousuarioestres2: numerousuarioestres2,
+        numerousuarioestres3: numerousuarioestres3,
+        numerousuarioestres4: numerousuarioestres4,
+        numerousuarioestres4: numerousuarioestres5,
+        numerousuarioestres4: numerousuarioestres6,
+
+        tiempopruebaestres1: tiempopruebaestres1,
+        tiempopruebaestres2: tiempopruebaestres2,
+        tiempopruebaestres3: tiempopruebaestres3,
+        tiempopruebaestres4: tiempopruebaestres4,
+        tiempopruebaestres5: tiempopruebaestres5,
+        tiempopruebaestres6: tiempopruebaestres6,
+
+        graficoestres1: graficoestres1,
+        //graficoestres2: graficoestres2,
+        //graficoestres3: graficoestres3,
+
+        criterioaceptacion: criterioaceptacion,
+        graficocriterioaceptacion: graficocriterioaceptacion
       });
   
       const blob = doc.getZip().generate({
@@ -108,6 +203,8 @@ function generateDocument() {
   
       saveAs(blob, "output.docx");
     };
+
+    reader.readAsDataURL(graficocarga1File);
   }
 
 function init() {
